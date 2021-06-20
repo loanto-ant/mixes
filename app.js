fe3bcc4c9f9033c7dfd2b85aec2d2516b1650900
loanto-ant <lorenz.filla@gmail.com>
2021-06-20 20:27:38 +0200

    included youtube search

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-function astronaut(){
-
+function youtube(){
+    var begriff = document.getElementById("yt_input").value;
+    begriff = "https://www.youtube.com/results?search_query=" + begriff;
+    document.getElementById("yt_link").setAttribute("href", begriff);
 }
